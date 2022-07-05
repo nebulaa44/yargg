ebfb1753b232154b8c7adf977b069eae2d52fbc6
kai <gmdezreal@gmail.com>
2022-07-05 16:35:26 -0500

    comment code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use colored::*;
+
 use rand::prelude::*;
 
 use std::cmp::Ordering::*;
@@ -6,13 +7,17 @@ use std::io::{Write, stdin, stdout};
 
 fn main() {
     let target = rand::thread_rng().gen_range(1..=100);
-    //println!("{target}");
 
     let mut guess: String = String::new();
     loop {
+        // reading stdin to guess doesn't clear it
+        // so we have to do it 
         guess.clear();
 
         print!("Enter your guess: ");
+        
+        // we need to flush the output so that the print above
+        // will actually happen
         stdout()
             .flush()
             .expect("Could not flush output");
@@ -21,9 +26,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Could not read guess");
 
+        // parse the guess into a u8
         let parse_result = guess.trim().parse::<u8>();
         let guess_parsed = match parse_result {
             Ok(v)    => v,
+
+            // setting to 101 will ignore the guess
+            // because of the next if statement
             Err(_) => 101
         };
 
