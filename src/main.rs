5667640245257b16e2a179dca06259f75b76e7e1
kai <gmdezreal@gmail.com>
2022-07-05 16:02:04 -0500

    get guess

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use rand::prelude::*;
 
+use std::io::{Read, Write, stdin, stdout};
+
 fn main() {
     let target = rand::thread_rng().gen_range(1..=100);
     println!("{target}");
+
+    let mut guess: String = String::new();
+    loop {
+        print!("Enter your guess: ");
+        stdout()
+            .flush()
+            .expect("Could not flush output");
+
+        stdin()
+            .read_line(&mut guess)
+            .expect("Could not read guess");
+    }
 }
